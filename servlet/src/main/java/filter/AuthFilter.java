0cc3c900be075d71a23184fa6ac5502c28d4527d
Anton Shtyk <th@tshtk.ru>
2022-04-13 12:37:10 +0300

    refactore: AuthFilter

--- servlet/src/main/java/filter/AuthFilter.java
+++ servlet/src/main/java/filter/AuthFilter.java
@@ -1,33 +1,31 @@
 package filter;
 
 import javax.servlet.ServletException;
-import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Optional;
 
-public class AuthFilter implements Filter {
+public class AuthFilter extends HttpFilter {
+
+    private static final String NAME_AUTH_COOKIE = "hh-auth";
+    private static final int LENGTH_AUTH_COOKIE = 10;
 
     @Override
-    public void doFilter(final ServletRequest rq, final ServletResponse rs, final FilterChain chain)
+    protected void doFilter(final HttpServletRequest rq, final HttpServletResponse rs, final FilterChain chain)
             throws IOException, ServletException {
-        HttpServletRequest httpRq = (HttpServletRequest) rq;
 
-        boolean authCookie = false;
+        Optional<Cookie[]> cookies = Optional.ofNullable(rq.getCookies());
+        boolean authCookie = cookies
+                .stream()
+                .flatMap(Arrays::stream)
+                .filter(o -> o.getName().equals(NAME_AUTH_COOKIE))
+                .anyMatch(o -> o.getValue().length() > LENGTH_AUTH_COOKIE);
 
-        Cookie[] cookies = httpRq.getCookies();
-
-        if (cookies != null) {
-            final String nameAuthCookie = "hh-auth";
-            final int lengthAuthCookie = 10;
-            authCookie = Arrays.stream(cookies)
-                    .filter(o -> o.getName().equals(nameAuthCookie))
-                    .anyMatch(o -> o.getValue().length() > lengthAuthCookie);
-        }
         rq.setAttribute("authCookie", authCookie);
         chain.doFilter(rq, rs);
     }
